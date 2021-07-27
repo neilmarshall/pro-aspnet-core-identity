7f32676ba958bdc73f6645b87cefb527f2a8810e
neilmarshall <neil.marshall@dunelm.org.uk>
2021-07-27 22:13:36 +0100

    initial chapter 7 updates

---
+++ IdentityApp/Pages/Identity/Admin/Dashboard.cshtml.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityApp.Pages.Identity.Admin
+{
+    public class DashboardModel : AdminPageModel
+    {
+        public int UsersCount { get; set; } = 0;
+        public int UsersUnconfirmed { get; set; } = 0;
+        public int UsersLockedOut { get; set; } = 0;
+        public int UsersTwoFactor { get; set; } = 0;
+    }
+}
