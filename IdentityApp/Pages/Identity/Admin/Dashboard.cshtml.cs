dd54040436c22abfe4ed096e474fb9c4d66a7acd
neilmarshall <neil.marshall@dunelm.org.uk>
2021-07-27 22:57:13 +0100

    seed users in database

--- IdentityApp/Pages/Identity/Admin/Dashboard.cshtml.cs
+++ IdentityApp/Pages/Identity/Admin/Dashboard.cshtml.cs
@@ -1,4 +1,9 @@
-﻿namespace IdentityApp.Pages.Identity.Admin
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Pages.Identity.Admin
 {
     public class DashboardModel : AdminPageModel
     {
@@ -6,5 +11,54 @@
         public int UsersUnconfirmed { get; set; } = 0;
         public int UsersLockedOut { get; set; } = 0;
         public int UsersTwoFactor { get; set; } = 0;
+
+        public UserManager<IdentityUser<int>> UserManager { get; private set; }
+
+        private readonly string[] emails =
+        {
+            "alice_app2@example.com",
+            "bob_app2@example.com",
+            "charlie_app2@example.com"
+        };
+
+        public DashboardModel(UserManager<IdentityUser<int>> userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public void OnGet()
+        {
+            UsersCount = UserManager.Users.Count();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            foreach (var existingUser in UserManager.Users.ToList())
+            {
+                if (existingUser.UserName.Contains("_app2"))
+                {
+                    var result = await UserManager.DeleteAsync(existingUser);
+                    result.Process(ModelState);
+                }
+            }
+
+            foreach (var email in emails)
+            {
+                var user = new IdentityUser<int>
+                {
+                    UserName = email,
+                    Email = email,
+                    EmailConfirmed = true
+                };
+
+                var result = await UserManager.CreateAsync(user);
+                result.Process(ModelState);
+            }
+
+            if (ModelState.IsValid)
+                return RedirectToPage();
+
+            return Page();
+        }
     }
 }
---
+++ IdentityApp/Pages/Identity/IdentityExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace IdentityApp.Pages.Identity
+{
+    public static class IdentityExtensions
+    {
+        public static bool Process(this IdentityResult result, ModelStateDictionary modelState)
+        {
+            foreach (var err in result.Errors ?? Enumerable.Empty<IdentityError>())
+            {
+                modelState.AddModelError(string.Empty, err.Description);
+            }
+
+            return result.Succeeded;
+        }
+    }
+}
